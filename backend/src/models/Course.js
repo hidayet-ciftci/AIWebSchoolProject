96a25567d45d2a6744240edc1f5298f97f9db6ab
Heyedos <hidosss@hotmail.com>
2025-12-12 20:45:37 +0300

    course api link

--- backend/src/models/Course.js
+++ backend/src/models/Course.js
@@ -2,11 +2,24 @@ const mongoose = require("mongoose");
 
 const courseSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  teacher: { type: String, required: true },
+  teacher: {
+    type: mongoose.Schema.Types.ObjectId, // Veri tipini, MongoDB'nin kimlik türü olan ObjectId olarak ayarla
+    ref: "Users", // Bu ObjectId'nin hangi modele ait olduğunu belirt (User.js'deki model adı "Users" idi)
+    required: true,
+  },
+  students: [
+    {
+      // Array kullanıyoruz, çünkü birden fazla öğrenci var.
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users", // Öğrenci de bir User olduğu için yine "Users" modeline referans veriyoruz
+    },
+  ],
   lessonNumber: { type: Number, required: true },
+  // Not: Artık öğrenci sayısını bu dizinin uzunluğundan alabileceğiniz için,
+  // 'studentNumber' alanı modelde gereksiz hale gelebilir.
   studentNumber: { type: Number },
   source: { type: String, default: "" },
   status: { type: String },
 });
 
-module.exports = mongoose.model("courses", courseSchema);
+module.exports = mongoose.model("courses", courseSchema); //
